6e24d06b7fc22767d7681f53750ce1deb79b87d9
itzzbas <74045534+itzzbas@users.noreply.github.com>
2022-03-29 12:59:37 +0200

    C# project toegevoegd, nu uitzoeken hoe ik deze backend aan kan roepen vanuit mijn VUE front-end

---
+++ VueAppBackEnd/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+namespace VueAppBackEnd
+{
+    public class Class1
+    {
+        public List<Article> Articles()
+        {
+            List<Article> Articles = new List<Article>();
+            var newsApiClient = new NewsApiClient("abdac52f05f64ef5805779553a2218e3");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "Oorlog",
+                SortBy = SortBys.Popularity,
+                Language = Languages.NL,
+                From = new DateTime(2018, 1, 25)
+            });
+            foreach (var article in articlesResponse.Articles)
+            {
+                Articles.Add(article);
+               
+            }
+            return Articles;
+        }
+    }
+}
+
