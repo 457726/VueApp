72e988dd720a1b000e73062eb7749b58361bb06e
itzzbas <74045534+itzzbas@users.noreply.github.com>
2022-04-08 13:51:44 +0200

    Components gebouwd en er voor gezorgd dat ik gebruik maak van de juiste bootstrap en dit kan toepassen.

--- VueApp/src/components/Home.vue
+++ VueApp/src/components/Home.vue
@@ -1,16 +1,35 @@
 <template>
+    <div>
+    <Navbar />
     <div class="home">
-        <h1>{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
+        <b-card title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2">
+            <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>    
     </div>
+        </div>
 </template>
 
 <script lang="ts">
     import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { BootstrapVue } from 'bootstrap-vue';
+    Vue.use(BootstrapVue)
+    import Navbar from './Navbar.vue'
 
-    @Component
-    export default class Home extends Vue {
-        @Prop() private msg!: string;
+    export default {
+        name: 'Home',
+        components: {
+            Navbar,
+        }
     }
 </script>
 
